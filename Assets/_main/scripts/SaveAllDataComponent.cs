21d8a3b74b44f4e3b67ddfce6b46806ee3afed8b
Djastas <timebrocop@gmail.com>
2023-10-16 18:52:20 +0500

    add save

---
+++ Assets/_main/scripts/SaveAllDataComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using _main.scripts.managers;
+using Sinbad;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _main.scripts
+{
+    public class SaveAllDataComponent : MonoBehaviour
+    {
+        [Button]
+        public void SaveAllData()
+        {
+            CsvUtil.SaveObject(SessionManager.Instance._connectsData,"TestSave.csv");
+
+        }
+    }
+}
